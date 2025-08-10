115d90c89ee064a3ed7732bb3887c49d2c652d83
deepbreath373 <lin1149735109@gmail.com>
2025-08-10 18:04:44 +0800

    chore: init

---
+++ src/main/java/com/talk/with/you/TalkWithYouApplication.java
@@ -0,0 +1,13 @@
+package com.talk.with.you;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkWithYouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalkWithYouApplication.class, args);
+    }
+
+}
